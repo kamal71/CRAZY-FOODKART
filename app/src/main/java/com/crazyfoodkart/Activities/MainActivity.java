5606024143af74f801bc62f4f9091db3cd49b116
KamalPanara <67721223+KamalPanara@users.noreply.github.com>
2020-08-16 15:23:36 +0530

    Added ChipBottomNavBar with Fragments

---
+++ app/src/main/java/com/crazyfoodkart/Activities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.crazyfoodkart.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.crazyfoodkart.Fragments.CostFragment;
+import com.crazyfoodkart.Fragments.HomeFragment;
+import com.crazyfoodkart.Fragments.ProfileFragment;
+import com.crazyfoodkart.Fragments.SearchFragment;
+import com.crazyfoodkart.R;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Widgets
+    private ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initializing all widgets
+        initializeWidgets();
+
+        // setting default item selected for ChipBottomNavBar
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        // Setting item selection Listener for ChipBottomNavBar
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i){
+                    case R.id.home:
+                        chipNavigationBar.setItemSelected(R.id.home, true);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+                        break;
+                    case R.id.search:
+                        chipNavigationBar.setItemSelected(R.id.search, true);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new SearchFragment()).commit();
+                        break;
+                    case R.id.cost:
+                        chipNavigationBar.setItemSelected(R.id.cost, true);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CostFragment()).commit();
+                        break;
+                    case R.id.profile:
+                        chipNavigationBar.setItemSelected(R.id.profile, true);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ProfileFragment()).commit();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void initializeWidgets(){
+        // We Will be Initializing our Widgets in this method.
+        chipNavigationBar = findViewById(R.id.chipNavBar);
+    }
+}
